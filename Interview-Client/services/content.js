9b5a5e1e36cee915b04cf33366d850855f7ee27f
Neel Dudhat <neeldudhat2004@gmail.com>
2026-04-17 17:13:56 +0530

    Add files via upload

---
+++ Interview-Client/services/content.js
@@ -0,0 +1,21 @@
+import axiosInstance from "./axiosInstance";
+
+export const fetchContentListApi = () => axiosInstance.get("/v1/question");
+
+export const fetchContentListByIdApi = ({ subCategoryId, param }) =>
+  axiosInstance.get(
+    `/v1/question/subCategory/${subCategoryId}?page=${param.page}&limit=${param.limit}`
+  );
+
+export const postQuestionApi = (data) => axiosInstance.post("/v1/question/", data);
+
+export const postAnswerApi = (data) => axiosInstance.post("/v1/answer/", data);
+
+export const getAnswerOfQuestionByIdApi = (question_id) => {
+  return axiosInstance.get(`/v1/answer/question/${question_id}`);
+};
+
+export const putAnswerOfQuestionApi = (data) =>
+  axiosInstance.put(`/v1/answer/${data.answer_id}`, data);
+
+export const getSuggestionApi = (data) => axiosInstance.get("/v1/suggestion", { params: data });
